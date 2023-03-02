0e59cd2e9e5d4fc478a5ea407b8c6c44b69f25dc
Darren Seah <darrenseahyongliang@protonmail.com>
2023-03-02 15:03:56 +0800

    New files to test Firebase auths. Working so far.

---
+++ test.js
@@ -0,0 +1,9 @@
+
+
+/* const ele = document.getElementById('registerButton');
+ele.addEventListener('click', (e) => {
+    e.preventDefault();
+    const emailVar = document.getElementById('emailField').value;
+    const passwordVar = document.getElementById('passwordField').value;
+    alert(emailVar);    
+}); */
