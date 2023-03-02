e9445126f62059632f9884036562eb69b2ba1946
Darren Seah <93176254+CodingGuin@users.noreply.github.com>
2023-03-02 16:07:50 +0800

    Updated scripts to js file

--- test.js
+++ test.js
@@ -1,9 +1,39 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBw1ySW4GfUqEeRtpBTYgCGeMSLC3ru4QU",
+    authDomain: "muworldv1-ft6.firebaseapp.com",
+    databaseURL: "https://muworldv1-ft6-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "muworldv1-ft6",
+    storageBucket: "muworldv1-ft6.appspot.com",
+    messagingSenderId: "659133217389",
+    appId: "1:659133217389:web:386e36e80f023670421d64",
+    measurementId: "G-BQJCVKPBJN"
+};
 
-/* const ele = document.getElementById('registerButton');
-ele.addEventListener('click', (e) => {
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
+document.getElementById('registerButton').addEventListener('click', (e) => {
     e.preventDefault();
     const emailVar = document.getElementById('emailField').value;
     const passwordVar = document.getElementById('passwordField').value;
-    alert(emailVar);    
-}); */
+
+    createUserWithEmailAndPassword(auth, emailVar, passwordVar)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+        alert('successfully created acc');
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        alert(errorMessage);
+    });
+});
