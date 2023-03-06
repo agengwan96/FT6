de7d2e1166a0cbc2855e61993dc306db6f94f3cc
Darren Seah <darrenseahyongliang@protonmail.com>
2023-03-06 13:45:23 +0800

    Updating Firebase lib

---
+++ init-firebase.js
@@ -0,0 +1,14 @@
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBw1ySW4GfUqEeRtpBTYgCGeMSLC3ru4QU",
+    authDomain: "muworldv1-ft6.firebaseapp.com",
+    databaseURL: "https://muworldv1-ft6-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "muworldv1-ft6",
+    storageBucket: "muworldv1-ft6.appspot.com",
+    messagingSenderId: "659133217389",
+    appId: "1:659133217389:web:386e36e80f023670421d64",
+    measurementId: "G-BQJCVKPBJN"
+};
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
