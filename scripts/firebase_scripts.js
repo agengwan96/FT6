2b80739cc396be4bcf58d1b53d098025fba40125
Darren Seah <darrenseahyongliang@protonmail.com>
2023-03-06 17:43:11 +0800

    Working registration auth + email verification

---
+++ scripts/firebase_scripts.js
@@ -0,0 +1,52 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBw1ySW4GfUqEeRtpBTYgCGeMSLC3ru4QU",
+    authDomain: "muworldv1-ft6.firebaseapp.com",
+    databaseURL: "https://muworldv1-ft6-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "muworldv1-ft6",
+    storageBucket: "muworldv1-ft6.appspot.com",
+    messagingSenderId: "659133217389",
+    appId: "1:659133217389:web:386e36e80f023670421d64",
+    measurementId: "G-BQJCVKPBJN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+registerForm.addEventListener('submit', (e) => {
+    if (emailField.value === '' || passwordField === '' ||
+        emailField.value === null || passwordField === null ||
+        ValidateMurdochEmail() === false) {
+        e.preventDefault();
+    }
+})
+
+document.getElementById('registerSubmitButton').addEventListener('click', (e) => {
+    e.preventDefault();
+    const emailVar = document.getElementById('emailRegisterField').value;
+    const passwordVar = document.getElementById('passwordRegisterField').value;
+
+    createUserWithEmailAndPassword(auth, emailVar, passwordVar)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+        alert('successfully created acc');
+
+        sendEmailVerification(user)
+        .then(() => {
+            // Email verification sent!
+            let msg = 'An email verification link has been sent to ' + user.email;
+            alert(msg);
+        });
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        alert(errorMessage);
+    });
+});
