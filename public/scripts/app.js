33b68a627d3ca449d15fc244be88b918e0b23d60
Darren Seah <darrenseahyongliang@protonmail.com>
2023-03-22 16:36:15 +0800

    Semi-working admin panel + Bug fixes + Deletion of unneeded files
    Big update. Semi-working admin panel with live update of unverified data. Verify and Reject buttons are unimplemented for now. Fixed small bugs. Removed Firebase modules cuz not needed. Hosting updated.

---
+++ public/scripts/app.js
@@ -0,0 +1,85 @@
+/* Variables */
+const form = document.getElementById('loginForm');
+const emailField = document.getElementById('emailTextField');
+const passwordField = document.getElementById('passwordTextField');
+let map;
+let r;
+let country;
+let weight;
+/*location object containing coordinates of country and count*/
+let locations = {
+    lati: 0,
+    long: 0,
+    count: 0
+};
+/*array that wil contain locations object*/
+let heatArr = [];
+
+/* Validates email to be an @murdoch.edu.au domain using regex */
+function ValidateMurdochEmail() {
+    var mailformat = /^\w+([\.-]?\w+)*@murdoch.edu.au+$/;
+    if (emailField.value.match(mailformat)) {
+        alert("VALID EMAIL");
+        return true;
+    }
+    alert("INVALID EMAIL");
+    emailField.focus();
+    return false;
+}
+
+/* Listener for form. Calls ValidateMurdochEmail() function and checks for authentcity.
+   Need to modify this for authenticating with Firebase. */
+/* form.addEventListener('submit', (e) => {
+    if (emailField.value === '' || passwordField === '' ||
+        emailField.value === null || passwordField === null ||
+        ValidateMurdochEmail() === false) {
+        e.preventDefault();
+    }
+}) */
+
+/* Mobile menu functions */
+function openMenu(){
+    document.body.classList += " menu--open";
+    console.log("success");
+}
+
+function closeMenu(){
+    document.body.classList.remove('menu--open');
+}
+
+/* Google Maps API */
+function initMap(){
+    map = new google.maps.Map(document.getElementById("map"), {center: {lat: 1.3421, lng: 103.9198}, 
+    zoom: 2, });
+}
+
+window.initMap = initMap;
+
+/*
+    GeoCode
+*/
+function geocode(country, weight){
+    axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
+        params: {
+            address: country,
+            key: "AIzaSyCjdKyrFDIIxYLcHJ2C5_4QV-bdfF7ZWTE"
+        }
+    })
+    .then(function(resp){
+            locations.lati = resp.data.results[0].geometry.location.lat;
+            location.long = resp.data.results[0].geometry.location.lng;
+            locations.count = weight;
+            add(locations);
+    })
+    .catch(function(err){
+        console.log("Error for geocode");
+    });
+}   
+
+function add(r){
+    heatArr.push(r);
+    let c = 0;
+    console.log(heatArr[c]);
+    c = c + 1;
+
+}
---
+++ public/scripts/modules.js
@@ -0,0 +1,86 @@
+    // Firebase App (the core Firebase SDK) is always required and must be listed first
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+    import { getAuth, signOut, onAuthStateChanged, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js";
+    import { getDatabase, ref, set, onValue, update } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
+  
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyBw1ySW4GfUqEeRtpBTYgCGeMSLC3ru4QU",
+        authDomain: "muworldv1-ft6.firebaseapp.com",
+        databaseURL: "https://muworldv1-ft6-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "muworldv1-ft6",
+        storageBucket: "muworldv1-ft6.appspot.com",
+        messagingSenderId: "659133217389",
+        appId: "1:659133217389:web:386e36e80f023670421d64",
+        measurementId: "G-BQJCVKPBJN"
+    };
+  
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth();
+    const database = getDatabase();
+  
+    // Login
+    const loginEmail = document.getElementById('loginEmail');
+    const loginPassword = document.getElementById('loginPassword');
+  
+    // Login Button Event Listener
+    if (document.getElementById('loginButton') != null) {
+      document.getElementById('loginButton').addEventListener('click', (e) => {
+        e.preventDefault();
+            signInWithEmailAndPassword(auth, loginEmail.value, loginPassword.value)
+                .then((userCredential) => {
+                    // Signed in 
+                    const user = userCredential.user;
+                    
+                    // Update last login  
+                    update(ref(database, 'users/' + user.uid), {
+                        lastLogin: new Date().toLocaleString(),
+                    });
+    
+                    // Alert success (remove later)
+                    alert('Login Successful');
+    
+                    // Redirect to dashboard
+                    window.location.href = './pages/dashboard.html';
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    alert('Login Failed');
+                });
+    });
+  }
+
+  // Get the current user
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      alert('User is signed in');
+
+      const userRef = ref(database, 'users/' + uid);
+      onValue(userRef, (snapshot) => {
+        const data = snapshot.val();
+        document.getElementById('userName').innerHTML = data.username;
+      });
+      
+    } else {
+      // User is signed out
+      alert('User is not signed in');
+    }
+  });
+
+  // Logout Button Event Listener
+  if (document.getElementById('logoutButton') != null) {
+    document.getElementById('logoutButton').addEventListener('click', (e) => {
+      e.preventDefault();
+      signOut(auth).then(() => {
+        // Sign-out successful.
+        alert('Logout Successful');
+        window.location.href = '../index.html';
+      }).catch((error) => {
+        // An error happened.
+        alert('Logout Failed');
+      });
+    });
+}
