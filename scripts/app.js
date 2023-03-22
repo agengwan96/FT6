7c005c5109ff98ce949bd9b6d062ab6a34d102b8
Ageng <agengwan@icloud.com>
2023-03-23 02:27:41 +0800

    added heatmap function in app.js

--- scripts/app.js
+++ scripts/app.js
@@ -5,14 +5,11 @@ const form = document.getElementById('loginForm');
 const emailField = document.getElementById('emailTextField');
 const passwordField = document.getElementById('passwordTextField');
 let map;
-let r;
-let country;
-let weight;
+let addr;
 /*location object containing coordinates of country and count*/
 let locations = {
     lati: 0,
     long: 0,
-    count: 0
 };
 /*array that wil contain locations object*/
 let heatArr = [];
@@ -67,20 +64,20 @@ window.initMap = initMap;
 /*
     GeoCode
     *converts country name into latitude and longitude
-    *takes in country name:country and number of repeating country:weight
+    *takes in address
 */
-function geocode(country, weight){
+function geocode(addr){
     axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
         params: {
-            address: country,
+            address: addr,
             key: "AIzaSyCjdKyrFDIIxYLcHJ2C5_4QV-bdfF7ZWTE"
         }
     })
     .then(function(resp){
             locations.lati = resp.data.results[0].geometry.location.lat;
             locations.long = resp.data.results[0].geometry.location.lng;
-            locations.count = weight;
-            add(locations);
+            heatArr.push(locations);
+            heatmap(heatArr);
     })
     .catch(function(err){
         console.log("Error for geocode");
@@ -88,30 +85,24 @@ function geocode(country, weight){
 }   
 
 /*
-    ADD
-    *adds locations object from geocode function into heatArr array
-    *takes in locations object:r
-*/
-function add(r){
-    heatArr.push(r);
-    let c = 0;
-    console.log(heatArr[c]);
-    c = c + 1;
-}
-
-geocode("640 pasir ris drive 1", 10);
-
-/*
     HEATMAP
     *applies a heatmap layer over the map
 */
 function heatmap(l){
-    map = new google.maps.Map(document.getElementById("map"), {center: {lat: 1.3421, lng: 103.9198}, 
-    zoom: 2, });
-
-    let heatmapData = [
-        {location: new google.maps.LatLng(l[0].lati, l[0].long), weight: l[0].count}
-    ];
+    let heatmapData = [];
 
     let ind = l.length;
+    console.log(ind);
+
+    for(let i = 0;i < ind;i++){
+        heatmapData.push({location: new google.maps.LatLng(l[i].lati, l[i].long)});
+    }
+
+    let heatmap = new google.maps.visualization.HeatmapLayer({
+        data: heatmapData
+    });
+    heatmap.setMap(map);
 }
+
+geocode("Australia");
+geocode("Singapoe");
