53843616acafd24e7dc676790943b85c40d124bb
Darren Seah <darrenseahyongliang@protonmail.com>
2023-04-02 19:23:33 +0800

    MAJOR UPDATE
    Full list can be found in the tele chat.

--- scripts/firebase_scripts.js
+++ scripts/firebase_scripts.js
@@ -86,27 +86,93 @@ addClickListener('indIntSubmit', async (e) => {
 async function createUnverifiedInteractionsTable() {
     const unverifiedInteractionsTableBody = document.getElementById('unverifiedInteractionsTableBody');
     const querySnapshot = await getDocs(collection(db, "unverifiedInteractions"));
-    
-    querySnapshot.forEach((doc) => {
-        const rowData = doc.data();
-        console.log(doc.id, " => ", rowData);
+
+    // Convert the querySnapshot into an array of objects containing the document ID and data
+    const data = querySnapshot.docs.map(doc => ({ id: doc.id, data: doc.data() }));
+
+    // Sort the data array based on the 'date submitted' field
+    data.sort((a, b) => {
+        const dateA = a.data.dateSubmitted.toDate();
+        const dateB = b.data.dateSubmitted.toDate();
+        return dateA - dateB;
+    });
+
+    // Insert the sorted data into the table
+    data.forEach(({ id, data: rowData }) => {
         let row = unverifiedInteractionsTableBody.insertRow();
-        
+
         const fieldNames = ['staffName', 'staffRole', 'staffDepartment', 'staffContact', 'visitDate', 'visitPurpose', 'visitStatus', 'orgName', 'orgAddress', 'orgType', 'dateSubmitted'];
-        
+
         fieldNames.forEach((fieldName, index) => {
             let cell = row.insertCell(index);
             cell.innerHTML = rowData[fieldName];
+            if (index === 6) {
+                cell.innerHTML = rowData[fieldName].toUpperCase();
+                // If status is approved, change cell color to green
+                if (rowData[fieldName].toLowerCase() === 'scheduled') {
+                    cell.style.backgroundColor = 'lightblue';
+                }
+                // If status is rejected, change cell color to red
+                else if (rowData[fieldName].toLowerCase() === 'cancelled') {
+                    cell.style.backgroundColor = 'lightcoral';
+                } else {
+                    cell.style.backgroundColor = 'lightgreen';
+                }
+            }
+            // If dateSubmitted, format date
+            else if (index === 10) {
+                cell.innerHTML = rowData[fieldName].toDate().toLocaleDateString();
+            }
+            // If visitDate, format date
+            else if (index === 4) {
+                const date = new Date(rowData[fieldName]);
+                cell.innerHTML = date.toLocaleDateString();
+            }
         });
 
         let verifyButton = row.insertCell(11);
-        verifyButton.innerHTML = `<button class="btn btn-primary" onclick="verifyInteraction('${doc.id}')">Verify</button>`;
-        
+        verifyButton.innerHTML = `<button class="btn btn-primary" onclick="verifyInteraction('${id}')">Verify</button>`;
+
         let rejectButton = row.insertCell(12);
-        rejectButton.innerHTML = `<button class="btn btn-danger" onclick="rejectInteraction('${doc.id}')">Reject</button>`;
+        rejectButton.innerHTML = `<button class="btn btn-danger" onclick="rejectInteraction('${id}')">Reject</button>`;
     });
 }
 
+
+// Upon verfiying an interaction, move it to the verified interactions collection
+/* WIP */
+async function verifyInteraction(docId) {
+    const docRef = doc(db, "unverifiedInteractions", docId);
+    const docSnap = await getDoc(docRef);
+    const docData = docSnap.data();
+    try {
+        await setDoc(doc(db, "verifiedInteractions", docId), docData);
+        await deleteDoc(docRef);
+        alert('Interaction has been verified');
+        window.location.href = 'admin.html';
+    } catch (error) {
+        console.error("Error adding document: ", error);
+        alert('Error verifying interaction');
+    }
+};
+
+// Upon rejecting an interaction, move it to the rejected interactions collection
+async function rejectInteraction(docId) {
+    const docRef = doc(db, "unverifiedInteractions", docId);
+    const docSnap = await getDoc(docRef);
+    const docData = docSnap.data();
+    try {
+        await setDoc(doc(db, "rejectedInteractions", docId), docData);
+        await deleteDoc(docRef);
+        alert('Interaction has been rejected');
+        window.location.href = 'admin.html';
+    } catch (error) {
+        console.error("Error adding document: ", error);
+        alert('Error rejecting interaction');
+    }
+};
+
+// 
 addClickListener('unverifiedInteractionsTable', createUnverifiedInteractionsTable);
 
 // Loads table when page is loaded
--- scripts/modules.js
+++ scripts/modules.js
@@ -1,86 +1,156 @@
-    // Firebase App (the core Firebase SDK) is always required and must be listed first
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
-    import { getAuth, signOut, onAuthStateChanged, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js";
-    import { getDatabase, ref, set, onValue, update } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
-  
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-        apiKey: "AIzaSyBw1ySW4GfUqEeRtpBTYgCGeMSLC3ru4QU",
-        authDomain: "muworldv1-ft6.firebaseapp.com",
-        databaseURL: "https://muworldv1-ft6-default-rtdb.asia-southeast1.firebasedatabase.app",
-        projectId: "muworldv1-ft6",
-        storageBucket: "muworldv1-ft6.appspot.com",
-        messagingSenderId: "659133217389",
-        appId: "1:659133217389:web:386e36e80f023670421d64",
-        measurementId: "G-BQJCVKPBJN"
-    };
-  
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth();
-    const database = getDatabase();
-  
-    // Login
-    const loginEmail = document.getElementById('loginEmail');
-    const loginPassword = document.getElementById('loginPassword');
-  
-    // Login Button Event Listener
-    if (document.getElementById('loginButton') != null) {
-      document.getElementById('loginButton').addEventListener('click', (e) => {
-        e.preventDefault();
-            signInWithEmailAndPassword(auth, loginEmail.value, loginPassword.value)
-                .then((userCredential) => {
-                    // Signed in 
-                    const user = userCredential.user;
-                    
-                    // Update last login  
-                    update(ref(database, 'users/' + user.uid), {
-                        lastLogin: new Date().toLocaleString(),
-                    });
-    
-                    // Alert success (remove later)
-                    alert('Login Successful');
-    
-                    // Redirect to dashboard
-                    window.location.href = './pages/dashboard.html';
-                })
-                .catch((error) => {
-                    const errorCode = error.code;
-                    const errorMessage = error.message;
-                    alert('Login Failed');
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+import { getAuth, signOut, onAuthStateChanged, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js";
+import { getDatabase, ref, set, onValue, update } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBw1ySW4GfUqEeRtpBTYgCGeMSLC3ru4QU",
+    authDomain: "muworldv1-ft6.firebaseapp.com",
+    databaseURL: "https://muworldv1-ft6-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "muworldv1-ft6",
+    storageBucket: "muworldv1-ft6.appspot.com",
+    messagingSenderId: "659133217389",
+    appId: "1:659133217389:web:386e36e80f023670421d64",
+    measurementId: "G-BQJCVKPBJN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const database = getDatabase();
+
+// Login
+const loginEmail = document.getElementById('loginEmail');
+const loginPassword = document.getElementById('loginPassword');
+
+// Login Button Event Listener
+if (document.getElementById('loginButton') != null) {
+  document.getElementById('loginButton').addEventListener('click', (e) => {
+    e.preventDefault();
+        signInWithEmailAndPassword(auth, loginEmail.value, loginPassword.value)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                
+                // Update last login  
+                update(ref(database, 'users/' + user.uid), {
+                    lastLogin: new Date().toLocaleString(),
                 });
+
+                // Alert success (remove later)
+                alert('Login Successful');
+
+                // Redirect to dashboard
+                window.location.href = './pages/dashboard.html';
+            })
+            .catch((error) => {
+                //const errorCode = error.code;
+                //const errorMessage = error.message;
+                alert('Login Failed');
+            });
+  });
+}
+
+// Get the current user
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const uid = user.uid;
+    //alert('User is signed in');
+
+    const userRef = ref(database, 'users/' + uid);
+    onValue(userRef, (snapshot) => {
+      const data = snapshot.val();
+      document.getElementById('userName').innerHTML = data.username;
+    });
+    
+  } else {
+    // User is signed out
+    //alert('User is not signed in');
+  }
+});
+
+const adminLink = document.getElementById('adminPanel');
+
+// Check if user is admin
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const uid = user.uid;
+    const userRef = ref(database, 'users/' + uid);
+    onValue(userRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data.role == 'admin') {
+        adminLink.style.display = 'block';
+      } else {
+        adminLink.style.display = 'none';
+      }
     });
   }
+});
 
-  // Get the current user
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      const uid = user.uid;
-      //alert('User is signed in');
-
-      const userRef = ref(database, 'users/' + uid);
-      onValue(userRef, (snapshot) => {
-        const data = snapshot.val();
-        document.getElementById('userName').innerHTML = data.username;
-      });
-      
-    } else {
-      // User is signed out
-      //alert('User is not signed in');
-    }
-  });
-
-  // Logout Button Event Listener
-  if (document.getElementById('logoutButton') != null) {
-    document.getElementById('logoutButton').addEventListener('click', (e) => {
-      e.preventDefault();
-      signOut(auth).then(() => {
-        // Sign-out successful.
-        alert('Logout Successful');
-        window.location.href = '../index.html';
-      }).catch((error) => {
-        // An error happened.
-        alert('Logout Failed');
-      });
+// Logout Button Event Listener
+if (document.getElementById('logoutButton') != null) {
+  document.getElementById('logoutButton').addEventListener('click', (e) => {
+    e.preventDefault();
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      alert('Logout Successful');
+      window.location.href = '../index.html';
+    }).catch((error) => {
+      // An error happened.
+      alert('Logout Failed');
     });
+  });
 }
+
+// MOU Upload
+let dropArea = document.getElementById('drop-area');
+
+;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, preventDefaults, false)
+})
+
+function preventDefaults (e) {
+  e.preventDefault()
+  e.stopPropagation()
+}
+
+;['dragenter', 'dragover'].forEach(eventName => {
+  dropArea.addEventListener(eventName, highlight, false)
+})
+
+;['dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, unhighlight, false)
+})
+
+function highlight(e) {
+  dropArea.classList.add('highlight')
+}
+
+function unhighlight(e) {
+  dropArea.classList.remove('highlight')
+}
+
+dropArea.addEventListener('drop', handleDrop, false)
+
+function handleDrop(e) {
+  let dt = e.dataTransfer
+  let files = dt.files
+
+  handleFiles(files)
+}
+
+function uploadFile(file) {
+  let url = 'YOUR URL HERE'
+  let formData = new FormData()
+
+  formData.append('file', file)
+
+  fetch(url, {
+    method: 'POST',
+    body: formData
+  })
+  .then(() => { /* Done. Inform the user */ })
+  .catch(() => { /* Error. Inform the user */ })
+}
+
